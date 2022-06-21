ff0bd825406f7447db78d8bcdea5980314860cdf
carlosdiazz <c.diazadriann@gmail.com>
2022-06-21 08:17:33 -0400

    ds

---
+++ Fundamentos_Node/Readme.md
@@ -0,0 +1,8 @@
+### para correrla en Desarrollo en windows uso
+    - npx nodemon archivo.js
+
+### para correrla en Producion
+    - pm2 start archivo.js
+    - pm2 status
+    - pm2 log
+    - 
--- Fundamentos_Node/monohilo.js
+++ Fundamentos_Node/monohilo.js
@@ -12,3 +12,6 @@ console.log('Hola Mundo');
 console.log(`Tu nombre es: ${process.env.NAME || 'Anonimo'}`);
 console.log(`Ciudad es: ${process.env.CITY || 'Desconocida'}`);
 console.log(`Tu apellido es: ${process.env.APELLIDO || 'Anonimo'}`);
+console.log(`Tu edad es: ${process.env.EDAD || 'Desconocida'}`);
+console.log(`Tu email es: ${process.env.EMAIL || 'Desconocido'}`);
+console.log(`Tu telefono es: ${process.env.TELEFONO || 'Desconocido'}`);
